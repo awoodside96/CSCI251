c5c9beae92899cb157c7f0b9dc9d32130843e232
Joe Cumbo <mooman219@gmail.com>
2016-02-23 16:43:09 -0500

    Create a read and search task runnable

--- project1/src/Project.java
+++ project1/src/Project.java
@@ -1,6 +1,10 @@
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -29,7 +33,7 @@ public class Project {
             }
             files[i] = file;
         }
-
+        WordSearch wordSearch = new WordSearch(files, words);
     }
 
     public static class WordSearch {
@@ -40,8 +44,7 @@ public class Project {
         private final ExecutorService searchPool = Executors.newCachedThreadPool();
 
         /**
-         * Creates a new WordSearch object. A defensive copy is made for the
-         * input parameters.
+         * Creates a new WordSearch object.
          *
          * @param files the list of files to search from.
          * @param words the list of words to search in the files for.
@@ -52,8 +55,59 @@ public class Project {
         }
 
         public void run() {
-
+            for (int i = 0; i < files.length; i++) {
+                FileReadTask readTask = new FileReadTask(files[i], words);
+                readPool.submit(readTask);
+            }
         }
     }
 
+    public static class FileReadTask implements Runnable {
+
+        /**
+         * Each thread will make a defensive copy of the word list to prevent
+         * threads from modifying the data.
+         */
+        private final File file;
+        private final String[] words;
+
+        public FileReadTask(File file, String[] words) {
+            this.file = file;
+            this.words = Arrays.copyOf(words, words.length);
+        }
+
+        @Override
+        public void run() {
+            try {
+                List<String> lines = Files.readAllLines(file.toPath(), Charset.defaultCharset());
+            } catch (IOException ex) {
+                System.err.println("An error occured while reading the file '" + file.getName() + "'.");
+            }
+        }
+    }
+
+    public static class FileSearchTask implements Runnable {
+
+        private final String fileName;
+        private final String word;
+        private final String lword;
+        private final List<String> lines;
+
+        public FileSearchTask(String fileName, String word, List<String> lines) {
+            this.fileName = fileName;
+            this.word = word;
+            this.lword = word.toLowerCase();
+            this.lines = lines;
+        }
+
+        @Override
+        public void run() {
+            for (String line : lines) {
+                if (line.toLowerCase().contains(lword)) {
+                    System.out.println(word + " " + fileName);
+                    return;
+                }
+            }
+        }
+    }
 }
