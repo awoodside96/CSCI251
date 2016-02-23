7e4ffa40fc9d4d73000b6240125563a9ace301ed
Joe Cumbo <mooman219@gmail.com>
2016-02-23 15:55:58 -0500

    Add a update script to sync with the server

---
+++ project1/src/Project.java
@@ -0,0 +1,14 @@
+
+/**
+ * @author Joseph Cumbo (jwc6999)
+ */
+public class Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ update.sh
@@ -0,0 +1,1 @@
+rsync -r --delete ../CSCI251/ jwc6999@glados.cs.rit.edu:/home/stu10/s16/jwc6999/courses/concurrent-csci251
