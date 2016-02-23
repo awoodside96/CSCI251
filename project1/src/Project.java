1a5ceaa8c8157f690cc9bda5386bbf556235a652
Joe Cumbo <mooman219@gmail.com>
2016-02-23 16:25:40 -0500

    Create a basic word search class and parse input

--- project1/src/Project.java
+++ project1/src/Project.java
@@ -1,4 +1,10 @@
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
 /**
  * @author Joseph Cumbo (jwc6999)
  */
@@ -8,7 +14,46 @@ public class Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        if (args.length < 2) {
+            System.out.println("Not enough arguments. Correct usage: Project [file,...] [word,...]");
+            return;
+        }
+        String[] fileNames = args[0].split(",");
+        String[] words = args[1].split(",");
+        File[] files = new File[fileNames.length];
+        for (int i = 0; i < fileNames.length; i++) {
+            File file = new File(fileNames[i]);
+            if (!file.exists()) {
+                System.err.println("The file '" + fileNames[i] + "' does not exist.");
+                return;
+            }
+            files[i] = file;
+        }
+
     }
-    
+
+    public static class WordSearch {
+
+        private final File[] files;
+        private final String[] words;
+        private final ExecutorService readPool = Executors.newCachedThreadPool();
+        private final ExecutorService searchPool = Executors.newCachedThreadPool();
+
+        /**
+         * Creates a new WordSearch object. A defensive copy is made for the
+         * input parameters.
+         *
+         * @param files the list of files to search from.
+         * @param words the list of words to search in the files for.
+         */
+        public WordSearch(File[] files, String[] words) {
+            this.files = Arrays.copyOf(files, files.length);
+            this.words = Arrays.copyOf(words, words.length);
+        }
+
+        public void run() {
+
+        }
+    }
+
 }
