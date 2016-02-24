8880cebadeb70abf1c5c037e80e8ec3490b6d2fd
Joe Cumbo <mooman219@gmail.com>
2016-02-24 12:52:22 -0500

    Use message passing for the data flow

--- project1/src/Project.java
+++ project1/src/Project.java
@@ -1,12 +1,12 @@
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 
 /**
@@ -17,96 +17,130 @@ public class Project {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         if (args.length < 2) {
             System.out.println("Not enough arguments. Correct usage: Project [file,...] [word,...]");
             return;
         }
         String[] fileNames = args[0].split(",");
-        String[] words = args[1].split(",");
-        File[] files = new File[fileNames.length];
+        String[] words = args[1].toLowerCase().split(",");
+        WordSearch search = new WordSearch(words);
         for (int i = 0; i < fileNames.length; i++) {
             File file = new File(fileNames[i]);
             if (!file.exists()) {
-                System.err.println("The file '" + fileNames[i] + "' does not exist.");
-                return;
+                System.out.println("The file '" + fileNames[i] + "' does not exist, skipping.");
+                continue;
             }
-            files[i] = file;
+            search.addFile(file);
         }
-        WordSearch wordSearch = new WordSearch(files, words);
+        search.catchup();
     }
 
-    public static class WordSearch {
+    public static final class Line {
 
-        private final File[] files;
-        private final String[] words;
-        private final ExecutorService readPool = Executors.newCachedThreadPool();
-        private final ExecutorService searchPool = Executors.newCachedThreadPool();
+        public final String line;
+        public final String fileName;
+
+        public Line(String line, String fileName) {
+            this.line = line;
+            this.fileName = fileName;
+        }
+    }
+
+    public static final class WordSearch {
+
+        private final LinkedBlockingQueue<File> fileList = new LinkedBlockingQueue<File>();
+        private final LinkedBlockingQueue<Line> lineList = new LinkedBlockingQueue<Line>();
+        private final ExecutorService readPool;
+        private final ExecutorService searchPool;
 
         /**
          * Creates a new WordSearch object.
          *
-         * @param files the list of files to search from.
-         * @param words the list of words to search in the files for.
+         * @param words the words to search for.
          */
-        public WordSearch(File[] files, String[] words) {
-            this.files = Arrays.copyOf(files, files.length);
-            this.words = Arrays.copyOf(words, words.length);
+        public WordSearch(String[] words) {
+            this.readPool = Executors.newWorkStealingPool();
+            this.searchPool = Executors.newWorkStealingPool();
+            for (int i = 0; i < words.length; i++) {
+                this.searchPool.submit(new SearchTask(words, lineList));
+                this.readPool.submit(new ReadTask(fileList, lineList));
+            }
         }
 
-        public void run() {
-            for (int i = 0; i < files.length; i++) {
-                FileReadTask readTask = new FileReadTask(files[i], words);
-                readPool.submit(readTask);
+        public final void addFile(File file) {
+            fileList.offer(file);
+        }
+
+        public final void catchup() throws InterruptedException {
+            int iterations = 0;
+            while (fileList.size() + lineList.size() > 0) {
+                Thread.sleep(10);
+                iterations++;
             }
+            System.out.println(iterations);
+//                System.out.println("Triggered " + fileList.size() + lineList.size());
         }
     }
 
-    public static class FileReadTask implements Runnable {
+    public static final class ReadTask implements Runnable {
 
-        /**
-         * Each thread will make a defensive copy of the word list to prevent
-         * threads from modifying the data.
-         */
-        private final File file;
-        private final String[] words;
+        private final LinkedBlockingQueue<File> fileList;
+        private final LinkedBlockingQueue<Line> lineList;
 
-        public FileReadTask(File file, String[] words) {
-            this.file = file;
-            this.words = Arrays.copyOf(words, words.length);
+        public ReadTask(LinkedBlockingQueue<File> fileList, LinkedBlockingQueue<Line> lineList) {
+            this.fileList = fileList;
+            this.lineList = lineList;
         }
 
         @Override
-        public void run() {
+        public final void run() {
             try {
-                List<String> lines = Files.readAllLines(file.toPath(), Charset.defaultCharset());
-            } catch (IOException ex) {
-                System.err.println("An error occured while reading the file '" + file.getName() + "'.");
+                while (true) {
+                    File file = fileList.take();
+                    try {
+                        BufferedReader reader = new BufferedReader(new FileReader(file));
+                        String fileName = file.getName();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            lineList.offer(new Line(line.toLowerCase(), fileName));
+                        }
+                    } catch (FileNotFoundException ex) {
+                        System.out.println("Unable to find file '" + file.getName() + "'.");
+                    } catch (IOException ex) {
+                        System.out.println("Problem while reading file file '" + file.getName() + "'.");
+                    }
+                }
+            } catch (InterruptedException ex) {
+                System.out.println("Interrupted file reader");
             }
         }
     }
 
-    public static class FileSearchTask implements Runnable {
+    public static final class SearchTask implements Runnable {
 
-        private final String fileName;
-        private final String word;
-        private final String lword;
-        private final List<String> lines;
+        private final String[] words;
+        private final LinkedBlockingQueue<Line> lineList;
 
-        public FileSearchTask(String fileName, String word, List<String> lines) {
-            this.fileName = fileName;
-            this.word = word;
-            this.lword = word.toLowerCase();
-            this.lines = lines;
+        public SearchTask(String[] words, LinkedBlockingQueue<Line> lineList) {
+            this.words = words;
+            this.lineList = lineList;
         }
 
         @Override
-        public void run() {
-            for (String line : lines) {
-                if (line.toLowerCase().contains(lword)) {
-                    System.out.println(word + " " + fileName);
-                    return;
+        public final void run() {
+            try {
+                while (true) {
+                    Line line = lineList.take();
+                    for (int i = 0; i < words.length; i++) {
+                        if (line.line.contains(words[i])) {
+                            System.out.println(words[i] + " " + line.fileName);
+                            continue;
+                        }
+                    }
                 }
+            } catch (InterruptedException ex) {
+                System.out.println("Interrupted word searcher");
             }
         }
     }
